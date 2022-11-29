3d1555a921513ffd559b02e6a9dbd4986ec1ad51
Graham Sabin <grahamcsabin@outlook.com>
2022-11-29 13:16:12 +0100

    testing off starter code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,148 @@
-import Image from "next/image";
-import { useState } from "react";
-import { createClient } from '@supabase/supabase-js';
+import Image from 'next/image'
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
 
 export async function getStaticProps() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-  );
+  )
 
-  const {data} = await supabaseAdmin
-    .from('images')
-    .select('*')
-    .order('id');
+  const { data } = await supabaseAdmin.from('images').select('*').order('id')
   return {
     props: {
-      images: data
+      images: data,
     },
   }
-
 }
 
 function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(' ')
 }
 
 type Image = {
   id: number
   href: string
   imageSrc: string
+  name: string
   year: string
-  description: string
 }
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-9">
-        {/* Images here */}
-        {images != null ?
-        images.map((image) => (
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
           <BlurImage key={image.id} image={image} />
-        ))
-      : null}
+        ))}
       </div>
     </div>
-    
-  ); 
+  )
 }
 
-function BlurImage({image}: {image: Image}) {
-  const [isLoading, setLoading] = useState(true);
-  
-  return(
-    <a href="#" className="group">
-      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true)
+
+  return (
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
           src={image.imageSrc}
           layout="fill"
           objectFit="cover"
           className={cn(
-            'group-hover:opacity-75 duration-700 ease-in-out',
+            'duration-700 ease-in-out group-hover:opacity-75',
             isLoading
-              ? 'gayscale blur-2xl scale-110'
-              : 'garyscale-0 blur-0 scale-100'
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
           )}
           onLoadingComplete={() => setLoading(false)}
-          />
+        />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@lee Rob</p>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.year}</p>
     </a>
   )
 }
+
+
+
+
+
+
+// import Image from "next/image";
+// import { useState } from "react";
+// import { createClient } from '@supabase/supabase-js';
+
+// export async function getStaticProps() {
+//   const supabaseAdmin = createClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+//     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+//   );
+
+//   const {data} = await supabaseAdmin
+//     .from('images')
+//     .select('*')
+//     .order('id');
+//   return {
+//     props: {
+//       images: data
+//     },
+//   }
+
+// }
+
+// function cn(...classes: string[]) {
+//   return classes.filter(Boolean).join(' ');
+// }
+
+// type Image = {
+//   id: number
+//   href: string
+//   imageSrc: string
+//   year: string
+//   description: string
+// }
+
+// export default function Gallery({ images }: { images: Image[] }) {
+//   return (
+//     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+//       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-9">
+//         {/* Images here */}
+//         {images != null ?
+//         images.map((image) => (
+//           <BlurImage key={image.id} image={image} />
+//         ))
+//       : null}
+//       </div>
+//     </div>
+    
+//   ); 
+// }
+
+// function BlurImage({image}: {image: Image}) {
+//   const [isLoading, setLoading] = useState(true);
+  
+//   return(
+//     <a href="#" className="group">
+//       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+//         <Image
+//           alt=""
+//           src={image.imageSrc}
+//           layout="fill"
+//           objectFit="cover"
+//           className={cn(
+//             'group-hover:opacity-75 duration-700 ease-in-out',
+//             isLoading
+//               ? 'gayscale blur-2xl scale-110'
+//               : 'garyscale-0 blur-0 scale-100'
+//           )}
+//           onLoadingComplete={() => setLoading(false)}
+//           />
+//       </div>
+//       <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+//       <p className="mt-1 text-lg font-medium text-gray-900">@lee Rob</p>
+//     </a>
+//   )
+// }
